294daae1bc75cb7a72cb7ce61cd41f03cdacf34d
Habib Ridho <habibridho@live.com>
2017-09-30 22:42:22 +0700

    create data model and unit test

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -11,26 +11,24 @@ import XCTest
 
 class FoodTrackerTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  // MARK: Meal class test
+  func testMealInitialization() {
+    let zeroRatingMeal = Meal.init(name: "Zero", photo: nil, rating: 0)
+    XCTAssertNotNil(zeroRatingMeal)
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    let highestRatingMeal = Meal.init(name: "Highest", photo: nil, rating: 5)
+    XCTAssertNotNil(highestRatingMeal)
+  }
+  
+  func testMealShouldFail() {
+    let negativeRatingMeal = Meal.init(name: "Negative", photo: nil, rating: -1)
+    XCTAssertNil(negativeRatingMeal)
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    let emptyStringMeal = Meal.init(name: "", photo: nil, rating: 0)
+    XCTAssertNil(emptyStringMeal)
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    let largeRatingMeal = Meal.init(name: "Large", photo: nil, rating: 6)
+    XCTAssertNil(largeRatingMeal)
+  }
     
 }
